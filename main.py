a81fea8beff963a5415d13d5d4cc9f48b2816157
Arun007 <65381427+EdwardElric07@users.noreply.github.com>
2020-11-09 21:07:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import spell_checker_and_suggester as scs
 
 word = input("Enter a word: ")
-suggested_word = scs.spell_check(word)
+suggested_word = scs.spell_check(word.lower())
 if suggested_word:
     print('You have spelled', word, 'correctly')
 else:
